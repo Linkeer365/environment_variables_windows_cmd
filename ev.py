3d91aedc8f1d607a33de43202fbae9129a2d16e4
Linkeer365 <564142445@qq.com>
2021-05-24 22:47:28 +0800

    2021-05-24_22:47:28+

--- ev.py
+++ ev.py
@@ -1,11 +1,12 @@
 import os
+import time
 
 # exit=0
 
 # 妈的 windows setx 自动truncate把我搞了个半死
 # 使用pathed来实现path的添加，具体见 http://p-nand-q.com/download/gtools/pathed.html
 
-cur_dir=os.getcwd()
+# cur_dir=os.getcwd()
 
 while 1:
 	ev_name=input("EV name:")
@@ -19,7 +20,13 @@ while 1:
 		if topath_or_not!='n':
 			# txt_path=f"{cur_dir}{os.sep}cc.txt"
 
-			addpath_comm=f"pathed /APPEND %{ev_name}% /USER"
+			# 这里不能用ev_name，因为windows比较操蛋的一点在于
+			# 他在程序没运行完之前不会自动把ev_name跟你的ev_value进行绑定
+
+			addpath_comm=f"pathed /APPEND {ev_value} /USER"
+
+			print("addpath:",addpath_comm)
+			time.sleep(5)
 			os.system(addpath_comm)
 			# with open(txt_path,"r",encoding="utf-8") as f:
 			# 	str1=f.read()
